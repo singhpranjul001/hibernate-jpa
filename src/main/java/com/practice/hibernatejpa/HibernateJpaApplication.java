a0d7ef2ac7726f87f1b5cc3cf12fbbfbde39ecb0
Pranjul Singh <singhpranjul001@gmail.com>
2024-04-18 18:10:17 +0530

    Created StudentDAO and StudentDAOImpl.

--- src/main/java/com/practice/hibernatejpa/HibernateJpaApplication.java
+++ src/main/java/com/practice/hibernatejpa/HibernateJpaApplication.java
@@ -1,5 +1,7 @@
 package com.practice.hibernatejpa;
 
+import com.practice.hibernatejpa.dao.StudentDAO;
+import com.practice.hibernatejpa.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,37 @@ public class HibernateJpaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner->{
-			System.out.println("Hello World!!");
+			//createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
 
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		//create multiple students
+		System.out.println("Creating 3 student object...");
+		Student tempStudent1 = new Student("Pranav", "Sangli", "sangli_cool@gmail.com");
+		Student tempStudent2 = new Student("Abhishek", "Yadav", "abhishekyadav11@gmail.com");
+		Student tempStudent3 = new Student("Priti", "Roy", "roypriti007@gmail.com");
+
+		//save the student objects
+		System.out.println("Saving the students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		//create the student object
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Rahul", "Sharma", "rahul.sharma@gmail.com");
+
+		//save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		//display the id of the save student
+		System.out.println("Saved student: Generated id: " + tempStudent.getId());
+	}
 }
---
+++ src/main/java/com/practice/hibernatejpa/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.practice.hibernatejpa.dao;
+
+import com.practice.hibernatejpa.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
