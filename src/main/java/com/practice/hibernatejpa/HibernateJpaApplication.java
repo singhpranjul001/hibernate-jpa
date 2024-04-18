a9651cfb1100bb08c19a6eadd0d1c947c47f8220
Pranjul Singh <singhpranjul001@gmail.com>
2024-04-18 15:48:34 +0530

    Initial commit

---
+++ src/main/java/com/practice/hibernatejpa/HibernateJpaApplication.java
@@ -0,0 +1,22 @@
+package com.practice.hibernatejpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner->{
+			System.out.println("Hello World!!");
+		};
+	}
+
+}
