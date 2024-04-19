a6800a00a74d0699332c446fcb6005d445fc33ea
Pranjul Singh <singhpranjul001@gmail.com>
2024-04-19 15:04:58 +0530

    Learned basic CRUD demo.

--- src/main/java/com/practice/hibernatejpa/HibernateJpaApplication.java
+++ src/main/java/com/practice/hibernatejpa/HibernateJpaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class HibernateJpaApplication {
 
@@ -18,8 +20,92 @@ public class HibernateJpaApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner->{
 			//createStudent(studentDAO);
-			createMultipleStudents(studentDAO);
+
+			//createMultipleStudents(studentDAO);
+
+			//readStudent(studentDAO);
+
+			//queryForStudent(studentDAO);
+
+			//queryForStudentByLastName(studentDAO);
+
+			//updateStudent(studentDAO);
+
+			//deleteStudent(studentDAO);
+
+			deleteAllStudents(studentDAO);
 		};
+	}
+
+	private void deleteAllStudents(StudentDAO studentDAO) {
+		System.out.println("Deleting All Students...");
+		int numRowsDeleted = studentDAO.deleteAll();
+		System.out.println("Deleted row count: " + numRowsDeleted);
+	}
+
+	private void deleteStudent(StudentDAO studentDAO) {
+		int studentId = 3;
+		System.out.println("Deleting Student id: " + studentId);
+		studentDAO.delete(studentId);
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+		//retrieve student based on the id: Primary Key
+		int studentId=1;
+		System.out.println("Getting the student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+
+		//change firstName to "Pranjul"
+		System.out.println("Updating the Student...");
+		myStudent.setFirstName("Pranjul");
+
+		//update the Student
+		studentDAO.update(myStudent);
+
+		//Display the updated student
+		System.out.println("Updated Student: " + myStudent);
+	}
+
+	private void queryForStudentByLastName(StudentDAO studentDAO) {
+		//get the list of Students
+		List<Student> theStudents = studentDAO.findByLastName("sangli");
+
+		//display the list
+		for(Student tempStudent : theStudents){
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void queryForStudent(StudentDAO studentDAO) {
+		//get a list of Students
+		List<Student> theStudents = studentDAO.findAll();
+
+		//display the list of Students
+		for(Student tempStudent : theStudents){
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+
+		//create a student object
+		System.out.println("Creating a new student...");
+		Student tempStudent=new Student("Poorav", "Jha", "jhaPoorav@gmail.com");
+
+		//save the student
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		//display id of the saved student
+		int theId= tempStudent.getId();
+		System.out.println("Saved Student. Generated id: " + theId);
+
+		//retrieve the student based on the id:Primary Key
+		System.out.println("Retrieving the Student with the given id: " + theId);
+		Student myStudent= studentDAO.findById(theId);
+
+		//display the student
+		System.out.println("Found the Student: " + myStudent);
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
--- src/main/java/com/practice/hibernatejpa/dao/StudentDAOImpl.java
+++ src/main/java/com/practice/hibernatejpa/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.practice.hibernatejpa.dao;
 
 import com.practice.hibernatejpa.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -22,5 +25,58 @@ public class StudentDAOImpl implements StudentDAO {
     @Transactional
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        //create a query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student ", Student.class);
+
+        /*For descending order by lastName
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName desc", Student.class);*/
+
+        //return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create the query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        //set the query parameters
+        theQuery.setParameter("theData", theLastName);
+
+        //display the query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        //retrieve the Student
+        Student theStudent = entityManager.find(Student.class, id);
+
+        //delete the Student
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int numRowsDeleted=entityManager.createQuery("DELETE FROM Student").executeUpdate();
+
+        return numRowsDeleted;
     }
 }
